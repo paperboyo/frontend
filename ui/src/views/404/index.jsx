fe641f25fa388cf24edc2fe0adca329ab2efe225
Alex Sanders <a@snd.rs>
2017-09-18 11:10:41 +0100

    merge master

--- ui/src/views/404/index.jsx
+++ ui/src/views/404/index.jsx
@@ -1,4 +1,4 @@
-// @follow
+// @flow
 import Logo from 'assets/images/guardian-logo-320.svg';
 import FourOhFour from 'assets/images/404.svg';
 import CTA from 'components/CTA';
@@ -13,35 +13,35 @@ import {
     link,
 } from './style.js.scss';
 
-export default ({ beaconUrl }: { beaconUrl: string }) =>
-<div style={body}>
-    <div style={wrapper}>
-        <a
-            href="https://www.theguardian.com/"
-            style={logo}
-        >
-            <Logo/>
-        </a>
-        <FourOhFour/>
-        <div style={contentBlock}>
-            <h1 style={heading}>
-                Sorry — the page you have requested does not exist
-            </h1>
-            <p style={bodyCopy}>
-                You may have followed an outdated link, or have mistyped a URL. If you believe this to be an error, please <a href="" style={link}>report it</a>.
-            </p>
-            <CTA
-                href="https://www.theguardian.com/"
-                color="brightBlue"
-                icon="./assets/images/arrow-right.svg"
-            >
-                Go to the Guardian home page
-            </CTA>
+export default ({ beaconUrl }: { beaconUrl: string }) => (
+    <div style={body}>
+        <div style={wrapper}>
+            <a href="https://www.theguardian.com/" style={logo}>
+                <Logo />
+            </a>
+            <FourOhFour />
+            <div style={contentBlock}>
+                <h1 style={heading}>
+                    Sorry — the page you have requested does not exist
+                </h1>
+                <p style={bodyCopy}>
+                    You may have followed an outdated link, or have mistyped a
+                    URL. If you believe this to be an error, please{' '}
+                    <a href="" style={link}>
+                        report it
+                    </a>.
+                </p>
+                <CTA
+                    href="https://www.theguardian.com/"
+                    color="brightBlue"
+                    icon="./assets/images/arrow-right.svg">
+                    Go to the Guardian home page
+                </CTA>
+            </div>
         </div>
-    </div>
-    <script
-        dangerouslySetInnerHTML={{
-            __html: `
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -51,14 +51,14 @@ export default ({ beaconUrl }: { beaconUrl: string }) =>
                 ga('set', 'dimension14', '404');
                 ga('send', 'pageview');
             `,
-        }}
-    />
+            }}
+        />
 
-    <script src="https://pasteup.guim.co.uk/js/lib/requirejs/2.1.5/require.min.js" />
+        <script src="https://pasteup.guim.co.uk/js/lib/requirejs/2.1.5/require.min.js" />
 
-    <script
-        dangerouslySetInnerHTML={{
-            __html: `
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
                 require.config({
                     paths: {
                         'ophan/http-status' : '//j.ophan.co.uk/ophan.http-status',
@@ -68,12 +68,13 @@ export default ({ beaconUrl }: { beaconUrl: string }) =>
                 reporter.reportStatus('next-gen', 404);
                 });
             `,
-        }}
-    />
-    <img
-        src={`${beaconUrl}/count/40x.gif`}
-        alt=""
-        style={{ display: 'none' }}
-        rel="nofollow"
-    />
-</div>;
+            }}
+        />
+        <img
+            src={`${beaconUrl}/count/40x.gif`}
+            alt=""
+            style={{ display: 'none' }}
+            rel="nofollow"
+        />
+    </div>
+);
